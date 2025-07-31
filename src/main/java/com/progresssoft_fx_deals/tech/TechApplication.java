539a87f8ad161f5b8278fa0d4ed0b03bc52453aa
soufianenajim99 <soufianenajim100@gmail.com>
2025-07-31 19:32:00 +0100

    initial project setup

---
+++ src/main/java/com/progresssoft_fx_deals/tech/TechApplication.java
@@ -0,0 +1,13 @@
+package com.progresssoft_fx_deals.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progresssoft_fx_deals/tech/TechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progresssoft_fx_deals.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
